6622859cb80e9d2076d362a9e5562cd3e1c2e2bf
remyatoney <remya.toney@whiterabbit.group>
2021-09-14 15:56:35 +0530

    first commit

---
+++ rocketman/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-*_cgb^drry_3avl#91@ied4qx0indo*)@c&d8_xtr7rp*o+0ax'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
